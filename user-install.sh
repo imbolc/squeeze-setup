c09e9921ce03f45ec1a7c5a780bac7c2d947beed
Imbolc <imbolc@imbolc.name>
2012-09-20 20:58:55 +0700

    mercurial, git configs and .bash_aliases

--- user-install.sh
+++ user-install.sh
@@ -15,5 +15,22 @@ git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 update-alternatives --set editor /usr/bin/vim.nox
 
 
-# Enable sudo autocomplete
+echo "=== Enable sudo autocomplete"
+
 echo "complete -cf sudo" >> ~/.bashrc
+
+echo "=== Bash aliases"
+cat > '~/.bash_aliases' << EOF
+alias nginx-restart='sudo /etc/init.d/nginx configtest && sudo /etc/init.d/nginx restart'
+alias chmod-standard="find ./ -type d | xargs chmod -v 755 ; find ./ -type f | xargs chmod -v 644"
+EOF
+
+echo "=== Mercurial config"
+cat > '~/.hgrc' << EOF
+[ui]
+username = $(whoami) <$(whoami)@$(hostname)>
+EOF
+
+echo "=== Git config"
+git config --global user.name $(whoami)
+git config --global user.email $(whoami)@$(hostname)
